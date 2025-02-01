639ac7797bfe8751a6a9a48b74610fe1eb200ba1
Patrick Henry Coppock <coppock@cmu.edu>
2025-02-01 15:01:15 -0500

    TensorRT-LLM has some weird behavior

--- share/vllm_.py
+++ share/vllm_.py
@@ -3,22 +3,22 @@ import random
 from common import *
 
 parser = ArgumentParser()
-parser.add_argument('engine')
-parser.add_argument('tokenizer')
+parser.add_argument('model')
+# parser.add_argument('tokenizer')
 parser.add_argument('-l', '--input-length', default=64, type=int)
-parser.add_argument('-t', '--max-tokens', type=int)
 args = parser.parse_args()
 
 # This should happen before importation of tensorrt_llm in order to handle
 # stdout abuse.
 iter = Iterator(args.iteration_count)
 
-from tensorrt_llm.llmapi import LLM, KvCacheConfig, SamplingParams
+from vllm import LLM, SamplingParams, TokensPrompt
 
-kv_cache_config = KvCacheConfig(max_tokens=args.max_tokens)
-llm = LLM(args.engine, args.tokenizer, kv_cache_config=kv_cache_config)
+llm = LLM(model=args.model, enforce_eager=True)
 sampling_params = SamplingParams(max_tokens=1)
 
 for _ in iter:
-    prompt = [random.randint(0, 2**31 - 1) for _ in range(args.input_length)]
+    prompt_token_ids = [random.randint(0, llm.get_tokenizer().vocab_size - 1)
+                        for _ in range(args.input_length)]
+    prompt = TokensPrompt(prompt_token_ids=prompt_token_ids)
     llm.generate(prompt, sampling_params=sampling_params, use_tqdm=False)
