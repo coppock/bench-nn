f4fbeceff8852442f60c30d94c3f1684fac11841
Patrick Henry Coppock <coppock@cmu.edu>
2025-02-25 19:33:06 -0500

    Support named pipe output

--- share/trtllm.py
+++ share/trtllm.py
@@ -1,30 +1,32 @@
+from argparse import ArgumentParser
 import itertools
-import os
 import signal
 import sys
 import time
-from argparse import ArgumentParser
 
-stdout = os.fdopen(os.dup(sys.stdout.fileno()), 'w')
-os.dup2(sys.stderr.fileno(), sys.stdout.fileno())
-# For some reason, this import must be outside the main function; otherwise,
-# the program will hang after completion.
 from tensorrt_llm.llmapi import LLM, SamplingParams, KvCacheConfig
 
 
-# For some reason, we can't just put the main body directly in the if block;
-# otherwise, the program will hang after completion.
-def main():
-    done = False
+done = False
 
-    def handler(self, *_):
-        done = True
+
+def handler(self, *_):
+    global done
+    done = True
+
+
+def main():
+    '''
+    For some reason, we can't just put the main body directly in the if block;
+    otherwise, the program will hang after completion.
+    '''
     for signum in signal.SIGTERM, signal.SIGINT:
         signal.signal(signum, handler)
 
     parser = ArgumentParser()
     parser.add_argument('model')
     parser.add_argument('-c', '--max-tokens', default=8192, type=int)
+    parser.add_argument('-f', '--file')
     parser.add_argument('-l', '--input-length', default=64, type=int)
     parser.add_argument('-m', '--iteration-count', type=int)
     parser.add_argument('-n', '--batch-size', default=1, type=int)
@@ -36,6 +38,7 @@ def main():
               kv_cache_config=KvCacheConfig(max_tokens=args.max_tokens))
     prompt = args.batch_size * [[0 for _ in range(args.input_length)]]
     sampling_params = SamplingParams(max_tokens=1)
+    file = open(args.file, 'w') if args.file else sys.stdout
     for _ in (range(args.iteration_count) if args.iteration_count
               else itertools.count()):
         if done:
@@ -43,7 +46,7 @@ def main():
         t_i = time.time()
         llm.generate(prompt, sampling_params=sampling_params, use_tqdm=False)
         t_f = time.time()
-        print(t_f, t_f - t_i, file=stdout, flush=True)
+        print(t_f, t_f - t_i, file=file, flush=True)
 
 
 if __name__ == '__main__':
